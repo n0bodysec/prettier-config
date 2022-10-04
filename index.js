22ebc3b7ca97a796acf8d592f45c7aa8a6e7fa14
Nobody <git@n0bodysec.com>
2022-10-04 11:15:02 -0300

    feat: publish v1.0.0

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	quoteProps: 'as-needed',
+	jsxSingleQuote: false,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: 'always',
+};
